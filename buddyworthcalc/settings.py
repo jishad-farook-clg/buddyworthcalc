357bdd4edc354d6ef5d18bdc116780ad1beada3e
Jishad <study.jishad@gmail.com>
2025-08-02 15:58:50 +0530

    cors removed

--- buddyworthcalc/settings.py
+++ buddyworthcalc/settings.py
@@ -39,13 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders', 
+
     'calculator',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-     'corsheaders.middleware.CorsMiddleware', 
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
